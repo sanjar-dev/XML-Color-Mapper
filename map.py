17096036d330ae625da73e9bdccc521de63eac93
Sanjar Kasmamytov <skasmamytov@gmail.com>
2021-04-27 21:18:14 +0600

    added comments, links and gitignore

--- map.py
+++ map.py
@@ -9,10 +9,14 @@ from PIL import Image, ImageDraw, ImageColor
 configur = ConfigParser()
 configur.read('config.ini')
 
+# FORMATTING
 class col:
     OKGREEN = '\033[92m'
+    OKCYAN = '\033[96m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    HEADER = '\033[95m'
 def message(msg):
     print("• " + str(msg))
 def failed(msg):
@@ -24,11 +28,13 @@ def getColorRandom():
     randHex = hex(randint(60, 220))[2:]
     return str(randHex).zfill(2)
 
+# META DATA
 metaName = configur.get('meta', 'name')
 metaVersion = configur.get('meta', 'version')
 metaAuthor = configur.get('meta', 'author')
 metaLink = configur.get('meta', 'link')
 
+# CLI
 arguments = len(sys.argv) - 1
 amount = 1;
 if (arguments < amount):
@@ -39,6 +45,7 @@ folder = configur.get('options', 'folder') + '/'
 file = sys.argv[1]
 file = folder + file
 
+# XML PARSING
 xmlTree = ET.parse(file)
 xmlRoot = xmlTree.getroot()
 sqrRange = []
@@ -46,6 +53,7 @@ for tag in xmlRoot:
     sqrPath = tag.attrib
     sqrRange += [[(int(sqrPath.get('x')), int(sqrPath.get('y'))), (int(sqrPath.get('x')) + int(sqrPath.get('width')), int(sqrPath.get('y')) + int(sqrPath.get('height')))]]
 
+# IMAGE SETUP
 imgName = folder + xmlRoot.attrib.get('imagePath')
 if (path.exists(imgName)):
     imgData = Image.open(imgName)
@@ -55,6 +63,7 @@ if (path.exists(imgName)):
 else:
     failed("No image file with XML was found.")
 
+# IMAGE WRITING AND SAVING
 with Image.new(imgMode, imgSize, imgFill) as img:
     for sqr in sqrRange:
         sqrNow = [sqr[0], (sqr[1][0]-1, sqr[1][1]-1)]
@@ -64,5 +73,5 @@ with Image.new(imgMode, imgSize, imgFill) as img:
     img.save(imgName.replace('.png', '-map.png'), "PNG")
     success("File saved as " + imgName.replace('.png', '-map.png') + ".\n")
     message("Credits not required, but greatly appriciated!")
-    message(metaName+" "+metaVersion+" by "+metaAuthor+"\n")
-    message(metaLink)
+    message(col.BOLD+metaName+" "+metaVersion+" by "+metaAuthor+"\n")
+    message(col.OKCYAN+metaLink+col.ENDC)
